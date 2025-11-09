5efe27b941a7a822273a9fcaf6ff9e956b15ee5e
Pheeraphat Dherachaisuphakij <dpheeraphat2547@gmail.com>
2025-11-09 16:16:52 +0700

    setup Project

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1 class="btn btn-primary text-3xl font-bold underline">
+      Hello Oggy!
+    </h1>
+    
+  </div>
+</template>
